9843973ce9aa3e9c7b83a80f442d53c587bbbbb3
Divesh Sharma <45972450+divesh9892@users.noreply.github.com>
2026-03-09 19:13:18 +0530

    fixed CORS

--- app/main.py
+++ app/main.py
@@ -11,10 +11,11 @@ app = FastAPI(
 # Configure CORS so the Next.js frontend can communicate with this backend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"], # We will add production domains later
+    allow_origins=["http://localhost:10001"], # We will add production domains later
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["Content-Disposition"]
 )
 
 # Wire up the V1 API Router
